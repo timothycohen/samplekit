f4a5bf1838fad206d932462f872c3edd62efef40
Tim Cohen <contact@timcohen.dev>
2024-02-07 14:18:04 -0500

    tooling: rate limiter

--- sites/samplekit.dev/src/lib/utils/common/language.ts
+++ sites/samplekit.dev/src/lib/utils/common/language.ts
@@ -4,3 +4,9 @@ export const cap = <T extends string>(word: T): Capitalize<T> =>
 export const pluralize = (word: string, count: number): string => (count === 1 ? word : `${word}s`);
 
 export const indefiniteArticle = (word: string): 'a' | 'an' => (word[0]?.match(/[aeiou]/i) ? 'an' : 'a');
+
+export const toHumanReadableTime = (seconds: number) => {
+	if (seconds < 60) return `${seconds} seconds`;
+	if (seconds < 3600) return `${Math.floor(seconds / 60)} minutes`;
+	return `${Math.floor(seconds / 3600)} hours`;
+};
