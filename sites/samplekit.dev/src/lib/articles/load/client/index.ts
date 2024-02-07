e6c715b5025f7ce62915a63ee9137ffbbc0d30f6
timothycohen <contact@timcohen.dev>
2024-02-07 14:16:02 -0500

    dynamic loading for melt-ui like preview

---
+++ sites/samplekit.dev/src/lib/articles/load/client/index.ts
@@ -0,0 +1,2 @@
+export * from './demoRenderableComponents';
+export * from './demoRenderableMeta';
--- sites/samplekit.dev/src/lib/utils/common/types/index.ts
+++ sites/samplekit.dev/src/lib/utils/common/types/index.ts
@@ -1,1 +1,7 @@
+import type { ComponentType, SvelteComponent } from 'svelte';
+
 export * from './result';
+
+export type NoPropComponent = ComponentType<
+	SvelteComponent<Record<string, never>, Record<string, never>, Record<string, never>>
+>;
