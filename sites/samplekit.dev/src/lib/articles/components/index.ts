0c0c2f262501da1af8a262f3c1e7733fb13d2c78
timothycohen <contact@timcohen.dev>
2024-02-07 14:16:10 -0500

    remove demo defaults to simplify load process

--- sites/samplekit.dev/src/lib/articles/components/index.ts
+++ sites/samplekit.dev/src/lib/articles/components/index.ts
@@ -1,7 +1,6 @@
 export { default as Changelog } from './Changelog.svelte';
 export { default as CodeTopper } from './CodeTopper.svelte';
 export { default as DateLine } from './DateLine.svelte';
-export { default as LiveDemo } from './LiveDemo.svelte';
 export { default as PrevNext } from './PrevNext.svelte';
 export { default as TOC } from './TOC.svelte';
 export { default as Series } from './Series.svelte';
