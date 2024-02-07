6d13939ca2065b0bd8f7a6848993a05a3f8e7e70
Tim Cohen <contact@timcohen.dev>
2024-02-07 14:13:16 -0500

    pages: profile

--- sites/samplekit.dev/src/routes/account/profile/+page.svelte
+++ sites/samplekit.dev/src/routes/account/profile/+page.svelte
@@ -1,3 +1,17 @@
+<script lang="ts">
+	import ProfileCard from './ProfileCard.svelte';
+
+	export let data;
+</script>
+
 <div class="space-y-6">
 	<h1 class="text-h4">Profile</h1>
+	<ProfileCard
+		nameForm={data.nameForm}
+		user={data.user}
+		editingAvatar={false}
+		onEditorClicked={() => {
+			console.log('todo');
+		}}
+	/>
 </div>
