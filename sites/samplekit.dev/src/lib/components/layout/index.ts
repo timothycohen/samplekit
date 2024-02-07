f39c4a201c25a6f9abb08b44baddb943003ee0a1
timothycohen <contact@timcohen.dev>
2024-02-07 14:13:11 -0500

    components: AccountLayout

--- sites/samplekit.dev/src/lib/components/layout/index.ts
+++ sites/samplekit.dev/src/lib/components/layout/index.ts
@@ -1,1 +1,2 @@
+export { default as AccountLayout } from './AccountLayout.svelte';
 export { default as Header } from './Header.svelte';
--- sites/samplekit.dev/src/routes/appearance/+layout.svelte
+++ sites/samplekit.dev/src/routes/appearance/+layout.svelte
@@ -1,1 +1,12 @@
-<div class="page"><slot /></div>
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { AccountLayout } from '$lib/components';
+</script>
+
+{#if $page.data['user']}
+	<AccountLayout><slot /></AccountLayout>
+{:else}
+	<div class="page">
+		<slot />
+	</div>
+{/if}
