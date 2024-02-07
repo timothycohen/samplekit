55f52f6f1d4c6c02ac8e4784a615bcd1ceec3370
Tim Cohen <contact@timcohen.dev>
2024-02-07 14:14:01 -0500

    tooling: mdsvex (md preprocessor for Svelte)

--- sites/samplekit.dev/svelte.config.js
+++ sites/samplekit.dev/svelte.config.js
@@ -1,10 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import { sequence, preprocessMeltUI } from '@melt-ui/pp';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
+import { mdsvexConfig } from './mdsvex.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+	preprocess: sequence([
+		mdsvex(mdsvexConfig), // order matters: https://github.com/pngwn/MDsveX/issues/390#issuecomment-1700347137
+		vitePreprocess(),
+		preprocessMeltUI(),
+	]),
 	kit: {
 		adapter: adapter(),
 		alias: {
