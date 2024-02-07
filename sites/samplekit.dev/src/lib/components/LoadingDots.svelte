4dcea607a679d285f363f8dc0df2a42ad894a74f
Tim Cohen <contact@timcohen.dev>
2024-02-07 14:17:12 -0500

    components: cart

---
+++ sites/samplekit.dev/src/lib/components/LoadingDots.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let classes = 'bg-gray-12';
+	export { classes as class };
+	export let wrapperClasses = '';
+
+	const dots = 'mx-[1px] inline-block h-1 w-1 animate-blink rounded-full';
+</script>
+
+<span class="mx-2 inline-flex items-center {wrapperClasses}">
+	<span class="{dots} {classes}" />
+	<span class="{dots} animation-delay-[200ms] animate-blink {classes}" />
+	<span class="{dots} animation-delay-[400ms] {classes}" />
+</span>
