bf742429fb7f141c755410f98d4c846790a42435
timothycohen <contact@timcohen.dev>
2024-02-07 14:14:03 -0500

    tooling: autoSlug preprocessor

--- sites/samplekit.dev/svelte.config.js
+++ sites/samplekit.dev/svelte.config.js
@@ -2,6 +2,8 @@
 // @ts-nocheck
 
 import { sequence, preprocessMeltUI } from '@melt-ui/pp';
+// @ts-expect-error – missing types
+import autoSlug from '@svelte-put/preprocess-auto-slug';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
@@ -13,6 +15,13 @@ const config = {
 	preprocess: sequence([
 		mdsvex(mdsvexConfig), // order matters: https://github.com/pngwn/MDsveX/issues/390#issuecomment-1700347137
 		vitePreprocess(),
+		// https://github.com/vnphanquang/svelte-put/blob/main/packages/preprocessors/auto-slug/src/auto-slug.types.ts
+		autoSlug(() => ({
+			tags: ['h2', 'h3', 'h4', 'h5', 'h6'],
+			/** @param {{ filename: string, content: string }} param0 */
+			files: ({ filename }) => filename.endsWith('.svx'),
+			anchor: { content: '#', position: 'prepend' },
+		})),
 		preprocessMeltUI(),
 	]),
 	kit: {
