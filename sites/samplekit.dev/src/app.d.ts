fa3dee19bb0feec5de8648bc521ca89a0d5e3f40
Tim Cohen <contact@timcohen.dev>
2024-02-07 14:13:23 -0500

    dx: checked form actions

--- sites/samplekit.dev/src/app.d.ts
+++ sites/samplekit.dev/src/app.d.ts
@@ -29,6 +29,49 @@ declare global {
 			| import('@sveltejs/kit').ActionFailure<{ success?: undefined; fail: string }>
 			| { fail?: undefined; success: string }
 		>;
+
+		/** Restrict a form element's action to known endpoints */
+		namespace Form {
+			// search for export const actions: Actions =
+			type _query = `&${string}` | '';
+			type _next = `&next=${string}`;
+
+			type Action =
+				| string
+				| `/login?/loginWithPassword`
+				| `/signup?/signupWithPassword`
+				| `/account/delete?/deleteUserWithSeshConf`
+				| `/account/security/auth?/updatePassFromCurrPass`
+				| `/account/verify?/seshConfFromPassword${_next}`
+				| `/account/verify?/seshConfFromSMS${_next}`
+				| `/account/verify?/seshConfFromAuthenticator${_next}`
+				| `/account/verify/email?/sendEmailVeriToSeshConfEmailLink${_next}`
+				| `/change-to-google?/passwordToLinkGoogle`
+				| `/change-to-password?/changeToEmailPassProvider`
+				| `/email-verification?/resendEmailVeriToVerifyEmailLink`
+				| `/login/google?/passToGoogleOAuth`
+				| `/login/verify-mfa?/loginWithSMS`
+				| `/login/verify-mfa?/loginWithAuthenticator`
+				| `/logout?/logoutCurrent`
+				| `/logout?/logoutSingle`
+				| `/logout?/logoutAll`
+				| `/mfa/sms?/sendSMSVeri`
+				| `/mfa/update/register/authenticator?/registerMFA_Authenticator_WithSeshConf`
+				| `/mfa/update/register/sms?/SMSSetupFromSeshConf${_query}` // ?phone=${string} | '' for resend with JS disabled
+				| `/mfa/update/register/sms?/registerMFA_SMS_WithSeshConfAndSetupSMS`
+				| `/mfa/update/remove/${DB.MFAs.Kind}?/removeMFAWithSeshConf`
+				| `/password-reset?/emailPassReset`
+				| `/password-update/${string}?/createNewPassFromPwReset`
+				| `/account/profile?/updateName`;
+		}
+	}
+
+	namespace svelteHTML {
+		interface IntrinsicElements {
+			form: Omit<import('svelte/elements').HTMLFormAttributes, 'action'> & {
+				action: App.Form.Action;
+			};
+		}
 	}
 
 	interface Window {
