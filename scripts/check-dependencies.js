c283f8ccdac80caec1c3072d84c157b6d060da48
Tim Cohen <contact@timcohen.dev>
2024-02-07 14:10:12 -0500

    tooling: dependency script

---
+++ scripts/check-dependencies.js
@@ -0,0 +1,40 @@
+import { execSync } from 'node:child_process';
+
+/**
+ * Credit
+ * https://github.com/sveltejs/kit/blob/master/scripts/check-dependencies.js
+ */
+
+const output = execSync('pnpm list --recursive --depth Infinity --json', {
+	encoding: 'utf8',
+	maxBuffer: 250 * 1024 * 1024,
+});
+
+const packages = JSON.parse(output);
+
+const deps = new Map();
+
+function get_or_default(map, key, constructor) {
+	let current = map.get(key);
+	if (!current) {
+		map.set(key, (current = new constructor()));
+	}
+	return current;
+}
+
+function add_deps(parent, dependencies, is_dev) {
+	for (const [name, { version }] of Object.entries(dependencies)) {
+		if (version.startsWith('link:')) continue;
+		const versions = get_or_default(deps, name, Map);
+		const parents = get_or_default(versions, version, Set);
+		parents.add(parent + (is_dev ? ':dev' : ''));
+	}
+}
+
+for (const pkg of packages) {
+	if (pkg.dependencies) add_deps(pkg.name, pkg.dependencies);
+	if (pkg.devDependencies) add_deps(pkg.name, pkg.devDependencies, 'dev');
+}
+
+const duplicates = new Map([...deps].filter(([, versions]) => versions.size > 1));
+console.log(duplicates);
