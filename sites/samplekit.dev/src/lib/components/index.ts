249ac969e404af5bd7d67dcab943cfd23cf64a26
timothycohen <contact@timcohen.dev>
2024-02-12 16:22:02 -0500

    meta: seo meta tags

--- sites/samplekit.dev/src/lib/components/index.ts
+++ sites/samplekit.dev/src/lib/components/index.ts
@@ -4,6 +4,7 @@ export { default as TabPanelItem } from './TabPanelItem.svelte';
 export { default as TabPanels } from './TabPanels.svelte';
 export { default as LoadingDots } from './LoadingDots.svelte';
 export { default as Icon } from './Icon.svelte';
+export { default as SEO } from './SEO.svelte';
 
 export * from './input';
 export * from './layout';
