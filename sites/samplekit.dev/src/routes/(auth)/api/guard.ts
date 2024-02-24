bc5a15834d98ff5dfe13c9b381a6baba2fc7e0ac
Tim Cohen <contact@timcohen.dev>
2024-02-24 17:12:05 -0500

    endpoints: whitelist internal ip

--- sites/samplekit.dev/src/routes/(auth)/api/guard.ts
+++ sites/samplekit.dev/src/routes/(auth)/api/guard.ts
@@ -11,6 +11,20 @@ const log = (method: 'info' | 'error', log: { id: string; address: string; err_c
 	logger[method](log);
 };
 
+const isInternalIpAddr = (ip: string) => {
+	const ipParts = ip.split('.');
+	if (ipParts.length !== 4) return false;
+	const str2 = ipParts[1];
+	if (typeof str2 === 'undefined') return false;
+	const num2 = +str2;
+	if (Number.isNaN(num2)) return false;
+	return (
+		ipParts[0] === '10' ||
+		(ipParts[0] === '192' && str2 === '168') ||
+		(ipParts[0] === '172' && num2 >= 16 && num2 <= 31)
+	);
+};
+
 export const guardApiKey = async ({
 	id,
 	event,
@@ -30,7 +44,8 @@ export const guardApiKey = async ({
 	const { request, getClientAddress } = event;
 	const req = postReq.safeParse(await request.json().catch(() => ({})));
 
-	const openWhitelist = ipWhitelist.length === 1 && ipWhitelist[0]! === '*';
+	const acceptsAnyIp = ipWhitelist.length === 1 && ipWhitelist[0]! === '*';
+	const acceptsOnlyInternalIp = ipWhitelist.length === 1 && ipWhitelist[0]! === 'internal';
 
 	let err: null | { err_code: string; status: 400 | 403 | 429 } = null;
 	if (!req.success) {
@@ -41,8 +56,10 @@ export const guardApiKey = async ({
 		err = { err_code: 'rate_limited', status: 429 };
 	} else if (expectedKey === '' || expectedKey !== req.data.cron_api_key) {
 		err = { err_code: 'incorrect_key', status: 403 };
-	} else if (!openWhitelist && !ipWhitelist.includes(getClientAddress())) {
-		err = { err_code: 'incorrect_ip', status: 403 };
+	} else if (!acceptsAnyIp) {
+		const ip = getClientAddress();
+		if (acceptsOnlyInternalIp && !isInternalIpAddr(ip)) err = { err_code: 'incorrect_ip', status: 403 };
+		else if (!ipWhitelist.includes(ip)) err = { err_code: 'incorrect_ip', status: 403 };
 	}
 
 	if (err) {
