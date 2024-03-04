24884519dfbabadc623d48c5a2db087e4b6162f5
Tim Cohen <contact@timcohen.dev>
2024-03-04 15:22:16 -0500

    protectStagingDeployments hook

--- sites/samplekit.dev/src/hooks.server.ts
+++ sites/samplekit.dev/src/hooks.server.ts
@@ -1,11 +1,14 @@
 import '$lib/initServer';
 
 import { sentryHandle, handleErrorWithSentry } from '@sentry/sveltekit';
-import { type HandleServerError, type Handle, redirect } from '@sveltejs/kit';
+import { type HandleServerError, type Handle, redirect, error } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
+import { dev } from '$app/environment';
+import { PUBLIC_ORIGIN } from '$env/static/public';
 import { auth, createAuthMiddleware } from '$lib/auth/server';
 import { logger } from '$lib/logging/server';
 import { handleAccountRedirects } from '$routes/account/hooks.server';
+import { deploymentAccessController } from '$routes/deployment-access/controller';
 
 export type SessionHandler = ReturnType<
 	(typeof import('./lib/auth/server/createAuth').auth)['createSessionHandler']
@@ -62,7 +65,29 @@ const deleteLinkHeaders: Handle = async ({ event, resolve }) => {
 	return response;
 };
 
-export const handle = sequence(sentryHandle(), populateLocals, handleAccountRedirects, deleteLinkHeaders);
+const protectStagingDeployments: Handle = async ({ event, resolve }) => {
+	if (PUBLIC_ORIGIN === 'https://www.samplekit.dev') {
+		if (event.url.pathname === '/deployment-access') return redirect(302, '404');
+		return resolve(event);
+	}
+	if (
+		dev ||
+		PUBLIC_ORIGIN.startsWith('http://localhost:') ||
+		event.url.pathname === '/deployment-access' ||
+		(await deploymentAccessController.isAuthenticated({ cookies: event.cookies }))
+	)
+		return resolve(event);
+
+	return error(403, 'Forbidden');
+};
+
+export const handle = sequence(
+	sentryHandle(),
+	populateLocals,
+	protectStagingDeployments,
+	handleAccountRedirects,
+	deleteLinkHeaders,
+);
 
 export const handleError: HandleServerError = handleErrorWithSentry(async ({ error, event, status, message }) => {
 	const errorId = crypto.randomUUID();
