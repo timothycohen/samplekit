b3e246c98285efee2b417592cf2197e3de95a122
Tim Cohen <contact@timcohen.dev>
2024-03-06 16:16:15 -0500

    article: preprocessors

---
+++ sites/samplekit.dev/src/routes/articles/preprocessors/meta.data.ts
@@ -0,0 +1,12 @@
+import type { RawFrontMatter } from '$lib/articles/schema';
+
+export default {
+	title: 'Blog with Preprocessors',
+	implementationSlug: `articles/preprocessors/#interactive-demo`,
+	srcCodeHref: 'https://github.com/timothycohen/samplekit/blob/main/packages/markdown',
+	description: 'Use preprocessors to format themed code blocks and Markdown tables alongside Svelte components.',
+	publishedAt: new Date('2024-03-06'),
+	authors: [{ name: 'Tim Cohen', email: 'contact@timcohen.dev' }],
+	tags: ['preprocessors', 'blog', 'markdown', 'code highlighting', 'DX'],
+	featured: true,
+} satisfies RawFrontMatter;
