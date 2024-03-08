e2d9bffa70e112a0d2510e9af04e90fbf4a21d3f
Tim Cohen <contact@timcohen.dev>
2024-03-08 13:31:26 -0500

    article: generic-url-state-controller

---
+++ sites/samplekit.dev/src/routes/articles/generic-url-state-controller/+page.server.ts
@@ -0,0 +1,26 @@
+import { mdCodeBlockToRawHtml } from '@samplekit/markdown';
+
+const typedVariantFiles = (() => {
+	const titles = ['Shared', 'String', 'Bool And Flag', 'Number', 'Select', 'MinMax'];
+	const delimiters = [
+		'export const createStringParam',
+		'export const createNullableBoolParam',
+		'export const createNumParam',
+		'export const createSelectParam',
+		'const strToNum',
+	];
+	const pattern = new RegExp(`(${delimiters.map((delimiter) => `(?=${delimiter})`).join('|')})`);
+
+	const rawCode = import('/src/lib/stores/params/params.ts?raw').then(({ default: rawCode }) => {
+		return rawCode.split(pattern).filter((item) => item.trim() !== '');
+	});
+
+	return titles.map((title, i) => ({
+		title,
+		rawHTML: rawCode.then((r) => r[i]!).then((rawCode) => mdCodeBlockToRawHtml({ lang: 'ts', rawCode }).data!),
+	}));
+})();
+
+export const load = async () => {
+	return { typedVariantFiles };
+};
