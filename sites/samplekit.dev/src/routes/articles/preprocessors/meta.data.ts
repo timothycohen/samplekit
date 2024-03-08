be5ef032644c854f7362c6edb75186b0fd145066
Tim Cohen <contact@timcohen.dev>
2024-03-08 14:27:23 -0500

    article: add thumbnail to preprocessors

--- sites/samplekit.dev/src/routes/articles/preprocessors/meta.data.ts
+++ sites/samplekit.dev/src/routes/articles/preprocessors/meta.data.ts
@@ -7,6 +7,7 @@ export default {
 	description: 'Use preprocessors to format themed code blocks and Markdown tables alongside Svelte components.',
 	publishedAt: new Date('2024-03-06'),
 	authors: [{ name: 'Tim Cohen', email: 'contact@timcohen.dev' }],
+	imgSm: '/articles/preprocessors-thumbnail.png',
 	tags: ['preprocessors', 'blog', 'markdown', 'code highlighting', 'DX'],
 	featured: true,
 } satisfies RawFrontMatter;
