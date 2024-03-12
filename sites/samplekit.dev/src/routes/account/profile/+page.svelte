c06c2093caf55952eba6d3efe3acc59987c4a4c7
timothycohen <contact@timcohen.dev>
2024-03-12 18:14:52 -0400

    refactor: separate avatar / name editors

--- sites/samplekit.dev/src/routes/account/profile/+page.svelte
+++ sites/samplekit.dev/src/routes/account/profile/+page.svelte
@@ -1,36 +1,10 @@
 <script lang="ts">
-	import { createDialog, melt } from '@melt-ui/svelte';
-	import AvatarEditor from './AvatarEditor.svelte';
 	import ProfileCard from './ProfileCard.svelte';
 
 	export let data;
-
-	const updateAvatar = (img: DB.User['avatar']) => {
-		data.user.avatar = img;
-		editAvatarOpen.set(false);
-	};
-
-	const {
-		elements: { portalled, overlay, content },
-		states: { open: editAvatarOpen },
-	} = createDialog({ forceVisible: true });
 </script>
 
 <div class="space-y-6">
 	<h1 class="text-h4">Profile</h1>
-	<ProfileCard
-		nameForm={data.nameForm}
-		user={data.user}
-		editingAvatar={$editAvatarOpen}
-		onEditorClicked={() => editAvatarOpen.set(true)}
-	/>
-</div>
-
-<div use:melt={$portalled}>
-	{#if $editAvatarOpen}
-		<div use:melt={$overlay} class="modal-overlay" />
-		<div class="modal-content-position rounded-card overflow-hidden" use:melt={$content}>
-			<AvatarEditor avatar={data.user.avatar} onCancel={() => editAvatarOpen.set(false)} {updateAvatar} />
-		</div>
-	{/if}
+	<ProfileCard nameForm={data.nameForm} user={data.user} />
 </div>
