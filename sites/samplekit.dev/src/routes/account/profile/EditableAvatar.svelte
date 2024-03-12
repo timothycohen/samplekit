4d65a09bc411eebedda1b0c8b74065c073aa3fdb
Tim Cohen <contact@timcohen.dev>
2024-03-12 18:14:52 -0400

    refactor: separate avatar / name editors

---
+++ sites/samplekit.dev/src/routes/account/profile/EditableAvatar.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { createDialog, melt } from '@melt-ui/svelte';
+	import { Avatar } from '$lib/components';
+	import AvatarEditor from './AvatarEditor.svelte';
+
+	export let user: DB.User;
+
+	const updateAvatar = (img: DB.User['avatar']) => {
+		user.avatar = img;
+		editAvatarOpen.set(false);
+	};
+
+	const {
+		elements: { portalled, overlay, content },
+		states: { open: editAvatarOpen },
+	} = createDialog({ forceVisible: true });
+</script>
+
+<Avatar
+	editing={$editAvatarOpen}
+	avatar={user.avatar}
+	size={150}
+	editable
+	onEditorClicked={() => editAvatarOpen.set(true)}
+/>
+
+<div use:melt={$portalled}>
+	{#if $editAvatarOpen}
+		<div use:melt={$overlay} class="modal-overlay" />
+		<div class="modal-content-position rounded-card overflow-hidden" use:melt={$content}>
+			<AvatarEditor avatar={user.avatar} onCancel={() => editAvatarOpen.set(false)} {updateAvatar} />
+		</div>
+	{/if}
+</div>
