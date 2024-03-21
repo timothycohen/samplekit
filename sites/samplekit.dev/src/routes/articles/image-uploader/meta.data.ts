95aa9cbbe31a06cace2e300c9d761693d588e354
Tim Cohen <contact@timcohen.dev>
2024-03-21 15:03:22 -0400

    fix: srcCodeHref link

--- sites/samplekit.dev/src/routes/articles/image-uploader/meta.data.ts
+++ sites/samplekit.dev/src/routes/articles/image-uploader/meta.data.ts
@@ -4,7 +4,7 @@ export default {
 	title: 'Image Cropper And Uploader',
 	implementationSlug: '/account/profile',
 	srcCodeHref:
-		'https://github.com/timothycohen/samplekit/blob/main/sites/samplekit.dev/src/lib/cloudStorage/client/cropImgUploadController.svelte',
+		'https://github.com/timothycohen/samplekit/blob/main/sites/samplekit.dev/src/lib/cloudStorage/client/cropImgUploadController.ts',
 	description:
 		'Select an image, upload it to an AWS S3 Bucket with a progress indicator, moderate it with Rekognition, save it to the DB, and serve it via AWS Cloudfront.',
 	publishedAt: new Date('Wed Mar 20 16:37:01 2024 -0400'),
