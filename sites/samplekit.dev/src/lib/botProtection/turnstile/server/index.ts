0135db1d35dccd086fbd9fb5e65a673e7d802b24
Tim Cohen <contact@timcohen.dev>
2024-03-27 12:28:47 -0400

    fix: try/catch turnstile fetch

--- sites/samplekit.dev/src/lib/botProtection/turnstile/server/index.ts
+++ sites/samplekit.dev/src/lib/botProtection/turnstile/server/index.ts
@@ -1,4 +1,5 @@
 import { TURNSTILE_SECRET_KEY } from '$env/static/private';
+import { logger } from '$lib/logging/server';
 
 interface TurnstileTokenValidateResponse {
 	'error-codes': string[];
@@ -7,21 +8,32 @@ interface TurnstileTokenValidateResponse {
 	cdata: string;
 }
 
-export const validateTurnstile = async ({ clientToken, ip }: { clientToken: string; ip: string | null }) => {
-	const response = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
-		method: 'POST',
-		headers: { 'content-type': 'application/json' },
-		body: JSON.stringify({
-			secret: TURNSTILE_SECRET_KEY,
-			response: clientToken,
-			remoteip: ip,
-		}),
-	});
+export const validateTurnstile = async ({
+	clientToken,
+	ip,
+}: {
+	clientToken: string;
+	ip: string | null;
+}): Promise<{ success: boolean; error: string | null | undefined }> => {
+	try {
+		const response = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
+			method: 'POST',
+			headers: { 'content-type': 'application/json' },
+			body: JSON.stringify({
+				secret: TURNSTILE_SECRET_KEY,
+				response: clientToken,
+				remoteip: ip,
+			}),
+		});
 
-	const data: TurnstileTokenValidateResponse = await response.json();
+		const data: TurnstileTokenValidateResponse = await response.json();
 
-	return {
-		success: data.success,
-		error: data['error-codes']?.length ? data['error-codes'][0] : null,
-	};
+		return {
+			success: data.success,
+			error: data['error-codes']?.length ? data['error-codes'][0] : null,
+		};
+	} catch (err) {
+		logger.error(err);
+		return { success: false, error: 'fetch-error' };
+	}
 };
