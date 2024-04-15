71494c5efa0762426a44037313c530533629455c
timothycohen <contact@timcohen.dev>
2024-04-15 14:42:12 -0400

    fix: CodeTopper collapsed margin

--- sites/samplekit.dev/src/lib/articles/components/CodeTopper.svelte
+++ sites/samplekit.dev/src/lib/articles/components/CodeTopper.svelte
@@ -4,27 +4,44 @@
 	import Copy from './Copy.svelte';
 
 	export let title: string;
-	export let copy = true;
-	export let collapsed = false;
+	export let copyable = true;
+	export let initialCollapsed = false;
+
+	const collapsible = !!$$slots.default;
+	let collapsed = collapsible ? initialCollapsed : false;
+	$: showCopy = collapsed ? false : copyable;
+
+	let codeTopperEl: HTMLElement;
+
+	function getTextContent() {
+		const codeFragment = codeTopperEl?.nextElementSibling;
+		return codeFragment?.textContent;
+	}
 
 	const service = useCollapsedService();
 	if (service) {
-		service.onTrigger((newState) => (collapsed = newState));
+		service.onTrigger((newState) => {
+			if (collapsible) collapsed = newState;
+		});
 	}
 </script>
 
-<div class="code-topper" style={collapsed ? 'border-bottom-width: 1px; margin: 0 0 1.5rem 0;' : ''}>
+<div
+	bind:this={codeTopperEl}
+	class="code-topper"
+	style={collapsed ? 'border-bottom-width: 1px; margin: 0 0 1.5rem 0;' : ''}
+>
 	<span class="bg-gray-3 rounded-tl-card overflow-auto whitespace-nowrap px-4 py-2">{title}</span>
 	<div class="flex">
-		{#if copy}
+		{#if showCopy}
 			<span
-				class="{$$slots.default ? '' : 'rounded-tr-card'} text-gray-10 border-gray-9
+				class="{collapsible ? '' : 'rounded-tr-card'} text-gray-10 border-gray-9
 				dark:border-gray-5 bg-gray-3 hover:bg-gray-4 min-h-10 w-10 border-l -outline-offset-1"
 			>
-				<Copy />
+				<Copy {getTextContent} />
 			</span>
 		{/if}
-		{#if $$slots.default}
+		{#if collapsible}
 			<button
 				on:click={() => (collapsed = !collapsed)}
 				class="text-gray-10 bg-gray-3 hover:bg-gray-4 rounded-tr-card grid min-h-10 w-10 place-content-center -outline-offset-1"
