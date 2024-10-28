13a8571d9ac37f73b3ed561968c82718e45c267d
Tim Cohen <contact@timcohen.dev>
2024-10-28 18:46:20 -0400

    fix: hydration errors with chrome extension

--- sites/samplekit.dev/src/lib/articles/components/card/CardList.svelte
+++ sites/samplekit.dev/src/lib/articles/components/card/CardList.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	// TODO: the 'Video Speed Controller' Google Chrome extension is fighting Svelte and throwing an error
 	import { fly } from 'svelte/transition';
+	import { browser } from '$app/environment';
 	import type { FeatureCard } from '$lib/articles/load';
 
 	const { cards }: { cards: FeatureCard[] } = $props();
@@ -14,32 +14,44 @@
 		cards.forEach((item, i) => result[i % cols]!.push(item));
 		return result;
 	});
+
+	// the invisible SizePlaceholder allocates some space so the scrollbar doesn't jump around
+	// it also provides a visible fallback when JavaScript is disabled
+
+	// the browser check prevents hydration errors with extensions that inject into the video element
+	// (such as the 'Video Speed Controller' Google Chrome extension)
 </script>
 
 <svelte:window bind:innerWidth />
 
-<noscript>
-	<div class="flex flex-wrap justify-around gap-4">
+{#snippet SizePlaceholder()}
+	<div class="no-script-visible invisible flex flex-wrap justify-around gap-4">
 		{#each cards as { FeatureCard, metadata }}
 			<div class="max-w-96"><FeatureCard {metadata} /></div>
 		{/each}
 	</div>
-</noscript>
+{/snippet}
 
-{#if innerWidth !== 1}
-	<div in:fly={{ y: 20, delay: 100, duration: 400 }} class="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
-		{#each parts as part}
-			<div class="flex flex-col gap-5">
-				{#each part as { FeatureCard, metadata }}
-					<FeatureCard {metadata} />
-				{/each}
-			</div>
-		{/each}
-	</div>
+{#if browser}
+	{#if innerWidth !== 1}
+		<div in:fly={{ y: 20, duration: 400 }} class="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
+			{#each parts as part}
+				<div class="flex flex-col gap-5">
+					{#each part as { FeatureCard, metadata }}
+						<FeatureCard {metadata} />
+					{/each}
+				</div>
+			{/each}
+		</div>
+	{/if}
 {:else}
-	<div class="invisible flex flex-wrap justify-around gap-4">
-		{#each cards as { FeatureCard, metadata }}
-			<div class="max-w-96"><FeatureCard {metadata} /></div>
-		{/each}
-	</div>
+	{@render SizePlaceholder()}
 {/if}
+
+<noscript>
+	<style>
+		.no-script-visible {
+			visibility: visible;
+		}
+	</style>
+</noscript>
